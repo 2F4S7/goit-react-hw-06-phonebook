bd46ed3b26e680519a762e2e8047f4e61304beb5
Mikhail Prytychenko <nightsta1kerhf@gmail.com>
2021-08-04 01:32:44 +0300

    v.6.0.3

--- src/index.js
+++ src/index.js
@@ -5,4 +5,18 @@ import './index.css';
 import App from './App';
 import 'modern-normalize/modern-normalize.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux';
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+// import
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persist={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
