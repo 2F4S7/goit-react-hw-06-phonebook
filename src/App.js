f6fd5af1460b50f06de5082af0cd265ec008a4a0
Mikhail Prytychenko <nightsta1kerhf@gmail.com>
2021-08-04 17:58:07 +0300

    v.6.1.5

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import ContactForm from './components/ContactForm';
-import Btn from './header/btn';
+import Btn from './components/Section/header/btn';
 import Top from './components/Section/Top';
 import Bottom from './components/Section/Bottom';
 import ContactList from './components/ContactList';
 import Filter from './components/Filter';
-// import styles from './App.module.css';
 
 const App = () => {
   return (
     <div>
-      <Btn title="React Home Work Phonebook 06" />
+      <Btn title="React-Redux Home Work Phonebook 06" />
       <Top title="Phonebook">
         <ContactForm />
       </Top>
